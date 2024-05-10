0635dda2c0365394719977a31e185ed6ef1aae76
BLKSONOX <blksonox@gmail.com>
2024-05-11 01:41:05 +0200

    Javascript

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 /*
-    A JavaScript class outside of conventions*/
+    
+    A JavaScript class outside of webstandards conventions
+    looks like a C program
+    
+*/
 
 ____ = {
 
@@ -11,18 +15,20 @@ ____ = {
 
     */
     xhttp: new XMLHttpRequest(),
+    docs: {0:"html", 1:"css"},
 
     /*       
-        Why not just use classes? 
+        Why not just use CSS classes? 
         the identifiers are there for a reason, classes is generative
 
         generative means programatically changable 
         css id and class logic delinates here
+
     */
 
     
 
-    extractCSSTemplate: {
+    exec: {
         /* 
             
             use elements array as search table within the stylesheet
@@ -32,33 +38,79 @@ ____ = {
         
         */
         
-        /* A class inside a class also possible in regular languages, differently */
+        /* A JS class inside a JS class also possible in regular languages, differently */
         /* Also known as string arrays, string arrays can contain code and pointer addresses */
         /* use self to escape this scope */
 
-        _(){ return self.window.____.xhttp },
+        _(){ 
+            
+            /* regular forever loop here as event loop */
+            /* can direct towards one already running */
+
+            /* 
+                
+                Event calls are just ques in an array fired off if signal recived 
+                it's all the same as low level code
+
+                can do this without forever loop
+
+            */
+            xhttp = self.window.____.xhttp;
+            return  xhttp;
+        },
         
         subobject_: function (){
             
             /* here goes private objects inside local functions scope */
-
             /* Private object nest until code ran and returned */
+            eventObject = {}
             obj = { newobjects: "new objects"}
 
             /* here goes for loop to manipulate new objects that's not static */
             return( {nest: "object functions nesting" + obj} )
+
+            /* Event rotational objects and functions */
         },
         subobject: {
             text: "text"
         },
 
         
-    }
+    },
 
-    
+    /* 
+        just use JSON files HTML attributes are as old as HTML can be anything
+    */
+    models: {
+
+        m: self.window.____.exec._()
+
+    },
+
+    documents:{
+
+         
+
+    },
+
+    /* 
+        
+        my hex dump is not the same as your hex dump, also known as transcoding 
+        key here not the same as key on server etc
+        public pipe, private pipe, private pipe feeds public pipe
+        kinda like the economy
+
+        to do afterwards, it's all colours and none colours outside of
+
+    */
+    hexStream:{
+
+    },
     
 }
 
-console.log(____.extractCSSTemplate._())
+console.log(____.exec._());
+console.log(window);
 
 /* add return nesting, allows for private and public chain link class constructors */
+/* From here events and callback loops are very straight forward */
