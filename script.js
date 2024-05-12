cb19aad267fc2f42da5452cfe962a6a5e51cd145
BLKSONOX <blksonox@gmail.com>
2024-05-12 12:55:59 +0200

    notes changes

--- script.js
+++ script.js
@@ -91,6 +91,7 @@ ____ = {
 
             /* here goes for loop to manipulate new objects that's not static */
             /* in C this would be a type array with string arrays */
+            /* create private objects in C make it not return */
             return( {nest: "object functions nesting" + obj} )
 
             /* Event rotational objects and functions */
